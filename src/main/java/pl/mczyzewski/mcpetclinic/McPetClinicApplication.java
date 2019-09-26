5a5a3960e40c2bf4009d8fd8340b1f5b05a18c9f
Michal Czyzewski <czy.mich@gmail.com>
2019-09-26 12:24:12 +0200

    Initial commit

---
+++ src/main/java/pl/mczyzewski/mcpetclinic/McPetClinicApplication.java
@@ -0,0 +1,13 @@
+package pl.mczyzewski.mcpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McPetClinicApplication.class, args);
+    }
+
+}
