d894de23679b3a164ce9874c8a554f67964a7730
Michal Czyzewski <czy.mich@gmail.com>
2019-09-30 14:24:43 +0200

    move dependency to different pom.xml files

--- pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/PetType.java
@@ -1,7 +1,5 @@
 package pl.mczyzewski.mcpetclinic.model;
 
-import org.springframework.context.annotation.Primary;
-
 public class PetType {
 
     private String name;
--- pet-clinic-web/src/main/java/McPetClinicApplication.java
+++ pet-clinic-web/src/main/java/McPetClinicApplication.java
@@ -1,5 +1,3 @@
-package pl.mczyzewski.mcpetclinic;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
