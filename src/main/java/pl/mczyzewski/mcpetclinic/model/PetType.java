4dab3199a3db83eaf71fc7c49210dbeb7824fe96
Michal Czyzewski <czy.mich@gmail.com>
2019-09-30 14:03:38 +0200

    add POJO's. Closes #2

--- src/main/java/pl/mczyzewski/mcpetclinic/model/PetType.java
+++ src/main/java/pl/mczyzewski/mcpetclinic/model/PetType.java
@@ -1,4 +1,16 @@
 package pl.mczyzewski.mcpetclinic.model;
 
+import org.springframework.context.annotation.Primary;
+
 public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
