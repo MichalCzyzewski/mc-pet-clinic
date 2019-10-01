b1d0fcef90b12e3bd7d77d25cea0f00133702fe7
Michal Czyzewski <czy.mich@gmail.com>
2019-10-02 00:02:58 +0200

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package pl.mczyzewski.mcpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package pl.mczyzewski.mcpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
