8917c81370d129e22d0a9d59e24ad7746f98b7c3
Michal Czyzewski <czy.mich@gmail.com>
2019-10-05 14:39:53 +0200

    Adding Owner list and updated controller. Closes #16

--- pet-clinic-web/src/main/java/pl/mczyzewski/mcpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/mczyzewski/mcpetclinic/controllers/OwnerController.java
@@ -1,12 +1,24 @@
 package pl.mczyzewski.mcpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.mczyzewski.mcpetclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String listOfOwners() {
+    public String listOfOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
