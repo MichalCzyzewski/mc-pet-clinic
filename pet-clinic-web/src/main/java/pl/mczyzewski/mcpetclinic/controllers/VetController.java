fe66c8ec77786411d240879562eb3f38257c6325
Michal Czyzewski <czy.mich@gmail.com>
2019-10-10 23:10:35 +0200

    add wro4j plugin and config  from Spring Pet Clinic.Closes #24

--- pet-clinic-web/src/main/java/pl/mczyzewski/mcpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/mczyzewski/mcpetclinic/controllers/VetController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.mczyzewski.mcpetclinic.services.VetService;
 
+
 @Controller
 public class VetController {
 
