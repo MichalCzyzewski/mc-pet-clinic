946768fede54043d15519fd257f69ef3f629626b
Michal Czyzewski <czy.mich@gmail.com>
2019-10-15 14:44:51 +0200

    Added Specialties Map Impl. Closes #32

---
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package pl.mczyzewski.mcpetclinic.services;
+
+import pl.mczyzewski.mcpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty,Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package pl.mczyzewski.mcpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import pl.mczyzewski.mcpetclinic.model.Specialty;
+import pl.mczyzewski.mcpetclinic.services.SpecialtiesService;
+
+import java.util.Set;
+@Service
+public class SpecialityMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return super.findById();
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
