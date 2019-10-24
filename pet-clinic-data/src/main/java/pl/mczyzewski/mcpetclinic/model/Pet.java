ac217c26c899bfe7051c8a62b4a66a4402cb4780
Michal Czyzewski <czy.mich@gmail.com>
2019-10-24 12:53:17 +0200

    Converted Owners to JPA Entities. Closes #37

--- pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Pet.java
@@ -1,12 +1,25 @@
 package pl.mczyzewski.mcpetclinic.model;
 
-import java.time.LocalDate;
+import org.hibernate.annotations.MetaValue;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
