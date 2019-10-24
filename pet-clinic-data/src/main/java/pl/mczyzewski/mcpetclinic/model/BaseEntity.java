d566aead9745dfafbdaa47327ff1238e96ec8c3b
Michal Czyzewski <czy.mich@gmail.com>
2019-10-24 09:52:14 +0200

    add Base Entity. Closes #36

--- pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/BaseEntity.java
@@ -1,9 +1,15 @@
 package pl.mczyzewski.mcpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
