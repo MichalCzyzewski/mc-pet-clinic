5773531161c6927cd48a3c051c98df3ff6b93138
Michal Czyzewski <czy.mich@gmail.com>
2019-10-30 15:41:27 +0100

    add lombok to petclinicproject . Closes #51

--- pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/BaseEntity.java
@@ -1,6 +1,8 @@
 package pl.mczyzewski.mcpetclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.GeneratedValue;
@@ -10,7 +12,8 @@ import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 @Getter
 @Setter
-
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
--- pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Owner.java
@@ -5,14 +5,22 @@ import lombok.*;
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+    @Builder
+    public Owner(Long id,String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
 
     @Column(name = "address")
     private String address;
@@ -22,7 +30,7 @@ public class Owner extends Person {
 
     @Column(name = "telephone")
     private String telephone;
-    @OneToMany(cascade = CascadeType.ALL,mappedBy = "owner")
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
-    
+
 }
