fb457dfa640c8f1112e4a710cd73d4435128a0ff
Michal Czyzewski <czy.mich@gmail.com>
2019-11-08 13:22:25 +0100

    add OwnerControllerTest class

---
+++ pet-clinic-web/src/test/java/pl/mczyzewski/mcpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,19 @@
+package pl.mczyzewski.mcpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OwnerControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listOwners() {
+    }
+
+    @Test
+    void findOwners() {
+    }
+}
