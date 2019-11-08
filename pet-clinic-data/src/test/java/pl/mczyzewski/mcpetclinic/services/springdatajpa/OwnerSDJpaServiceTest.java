531f12ea7cd43c6b4c3bbfe877a902aa4f4c289a
Michal Czyzewski <czy.mich@gmail.com>
2019-11-08 13:09:29 +0100

    added jpa test for Owner services. Closes #50

--- pet-clinic-data/src/test/java/pl/mczyzewski/mcpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
+++ pet-clinic-data/src/test/java/pl/mczyzewski/mcpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -15,12 +15,10 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerSDJpaServiceTest {
@@ -78,17 +76,27 @@ class OwnerSDJpaServiceTest {
     }
 
     @Test
+    void findByIdNotFound() {
+        when(ownerRepository.findById(anyLong())).thenReturn(Optional.empty());
+        Owner owner = ownerService.findById(1L);
+        assertNull(owner);
+    }
+
+    @Test
     void save() {
         Owner ownerToSave = Owner.builder().id(1L).build();
         when(ownerRepository.save(any())).thenReturn(returnOwner);
         Owner saveOwner = ownerService.save(ownerToSave);
         assertNotNull(saveOwner);
+
+        verify(ownerRepository).save(any());
     }
 
     @Test
     void delete() {
         ownerService.delete(returnOwner);
-        verify(ownerRepository).delete(any());
+
+        verify(ownerRepository,times(1)).delete(any());
     }
 
     @Test
