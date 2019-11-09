518a0f3781385071bd7bf87a9f6f256fd1c8a580
Michal Czyzewski <czy.mich@gmail.com>
2019-11-09 23:05:44 +0100

    add test to OwnerControllerTest

--- pet-clinic-web/src/test/java/pl/mczyzewski/mcpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/pl/mczyzewski/mcpetclinic/controllers/OwnerControllerTest.java
@@ -6,11 +6,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import pl.mczyzewski.mcpetclinic.model.Owner;
 import pl.mczyzewski.mcpetclinic.services.OwnerService;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
@@ -22,14 +28,25 @@ class OwnerControllerTest {
 
     Set<Owner> owners;
 
+    MockMvc mockMvc;
     @BeforeEach
     void setUp() {
 
         owners = new HashSet<>();
+        owners.add(Owner.builder().id(1L).build());
+        owners.add(Owner.builder().id(2L).build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
     }
 
     @Test
-    void listOwners() {
+    void listOwners() throws Exception{
+        when(ownerService.findAll()).thenReturn(owners);
+
+        mockMvc.perform(get("/"))
+        .andExpect(status().is(200));
     }
 
     @Test
