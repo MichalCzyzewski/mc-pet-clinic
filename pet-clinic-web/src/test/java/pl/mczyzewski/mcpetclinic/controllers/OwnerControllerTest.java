fea69ea340c2465c20bc23d2c6baba5497f07ee2
Michal Czyzewski <czy.mich@gmail.com>
2019-11-09 22:40:45 +0100

    add OwnerControllerTest class

--- pet-clinic-web/src/test/java/pl/mczyzewski/mcpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/pl/mczyzewski/mcpetclinic/controllers/OwnerControllerTest.java
@@ -2,11 +2,30 @@ package pl.mczyzewski.mcpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.mczyzewski.mcpetclinic.model.Owner;
+import pl.mczyzewski.mcpetclinic.services.OwnerService;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+    @InjectMocks
+    OwnerController controller;
+
+    Set<Owner> owners;
 
     @BeforeEach
     void setUp() {
+
+        owners = new HashSet<>();
     }
 
     @Test
