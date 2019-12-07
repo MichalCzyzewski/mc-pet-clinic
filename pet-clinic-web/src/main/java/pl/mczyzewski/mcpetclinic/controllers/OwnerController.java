22332e64f5d58af3fbabc6dc734cd278d8a50092
Michal Czyzewski <czy.mich@gmail.com>
2019-12-07 02:14:04 +0100

    add display owner test

--- pet-clinic-web/src/main/java/pl/mczyzewski/mcpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/mczyzewski/mcpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package pl.mczyzewski.mcpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import pl.mczyzewski.mcpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
@@ -27,6 +30,13 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners() {
         return "notImplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+        ModelAndView mnv = new ModelAndView("owners/ownerDetails");
+        mnv.addObject(ownerService.findById(ownerId));
+        return mnv;
     }
 
 }
