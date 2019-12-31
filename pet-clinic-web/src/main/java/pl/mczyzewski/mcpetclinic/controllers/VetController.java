727d28741ea2b345bdcb46a0d12dfbfa98d640ac
Michal Czyzewski <czy.mich@gmail.com>
2019-12-31 02:04:40 +0100

    Implemented Vets JSON Endpoint . Closes #69

--- pet-clinic-web/src/main/java/pl/mczyzewski/mcpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pl/mczyzewski/mcpetclinic/controllers/VetController.java
@@ -3,9 +3,13 @@ package pl.mczyzewski.mcpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.mczyzewski.mcpetclinic.services.*;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.mczyzewski.mcpetclinic.model.Vet;
+import pl.mczyzewski.mcpetclinic.services.VetService;
 
+import java.util.Set;
 
 
 @Controller
@@ -23,5 +27,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody
+    Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
