32a7cd5d326d0f80e1b05b179d081dbd56a9d936
Michal Czyzewski <czy.mich@gmail.com>
2020-01-05 15:27:09 +0100

    sort code in Pet java

--- pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Pet.java
@@ -14,7 +14,7 @@ import java.util.Set;
 @AllArgsConstructor
 @Entity
 @Table(name = "pets")
-public class Pet extends BaseEntity{
+public class Pet extends BaseEntity {
 
     @Builder
     public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
@@ -24,7 +24,7 @@ public class Pet extends BaseEntity{
         this.owner = owner;
         this.birthDate = birthDate;
 
-        if(visits == null || visits.size() > 0) {
+        if (visits == null || visits.size() > 0) {
             this.visits = visits;
         }
     }
