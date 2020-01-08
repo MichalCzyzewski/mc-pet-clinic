53bdb2d0d38984924c29fc39d00ac0670793ba05
Michal Czyzewski <czy.mich@gmail.com>
2020-01-08 09:31:15 +0100

    delete space

--- pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Visit.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 @Builder
 @Entity
 @Table(name = "visits")
-public class Visit extends BaseEntity {
+public class Visit extends BaseEntity{
 
     @Column(name = "date")
     private LocalDate date;
