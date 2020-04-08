a00e894b2fd942ba643a56205628370c4187eaf5
Michal Czyzewski <czy.mich@gmail.com>
2020-04-08 23:50:18 +0200

    sort code

--- pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pl/mczyzewski/mcpetclinic/model/Person.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,11 +15,12 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public class Person extends BaseEntity {
 
-    public Person(Long id,String firstName, String lastName) {
+    public Person(Long id, String firstName, String lastName) {
         super(id);
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
